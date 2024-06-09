6515a26a37323dccf79d76e5d39dcfeb6eec1627
Steven J. von Dohlen <vondoste@live.unc.edu>
2024-06-09 16:28:26 -0500

    As of 247.  Runs on local K cluster.

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Fibonacci Calcualtor for Kubernetes.</h1>
+          <h1>Fibonacci Calculator for Kubernetes.</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
